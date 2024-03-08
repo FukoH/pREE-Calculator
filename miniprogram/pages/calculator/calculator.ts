9fe6a7001f73478cba5197d815d9036bd9c64fdd
FukoH <hsx500@163.com>
2024-03-08 23:23:52 +0800

    调整样式
    删除冗余代码和过大图片

--- miniprogram/pages/calculator/calculator.ts
+++ miniprogram/pages/calculator/calculator.ts
@@ -7,109 +7,147 @@ function isValidNumber(value: any) {
     return /^\d+(\.\d+)?$/.test(value) && parseFloat(value) > 0;
   }
 
-Component({
-    data: {
-        weight: 0,
-        height:0,
-        age: 0,
-        gripStrength: 0,
-        heartRate: 0,
-        gender: 'male',
-        formulas: [ // 不同公式名称数组
-            { name: '1BJH-ZHW1公式' },
-            { name: 'Alemán公式' },
-            { name: 'Lührmann1公式' },
-            { name: 'Harris-Benedict（HB公式）' },
-            { name: 'FAO-WHO-UNU（WHO公式）' },
-            { name: 'ESPEN公式' },
-            { name: 'CSPEN公式' }
-          ]
+  const formulas = [ 
+  { name: '1BJH-ZHW1公式' },
+  { name: 'Alemán公式' },
+  { name: 'Lührmann1公式' },
+  { name: 'Harris-Benedict（HB公式）' },
+  { name: 'FAO-WHO-UNU（WHO公式）' },
+  { name: 'ESPEN公式' },
+  { name: 'CSPEN公式'}
+]
+
+Page({
+  data: {
+    form: {
+      age: '',
+      height: '',
+      weight: '',
+      heartRate:'',
+      gripStrength:'',
+      gender:''
     },
-    methods: {
-        bindWeightInput: function (e: any) {
-            const value = e.detail.value;
-
-            if (!isValidNumber(value)) {
-                wx.showToast({
-                  title: '请输入正确的体重',
-                  icon: 'none'
-                });
-                return;
-              }
-            this.setData({
-                weight: e.detail.value
-            });
+    showDialog: false,
+    buttonContent: [{text: '确定'}],
+    items: [
+        {name: '男', value: 'male',checked:false},
+        {name: '女', value: 'female',checked:false}
+    ],
+    // items: [
+    //     { name: 'USA', value: '美国' },
+    //     { name: 'CHN', value: '中国', checked: true},
+    //     { name: 'BRA', value: '巴西' },
+    //     { name: 'JPN', value: '日本' },
+    //     { name: 'ENG', value: '英国' },
+    //     { name: 'FRA', value: '法国' },
+    //   ],
+    errorMsg: '', 
+    formulas: formulas,
+    rules: [
+        {
+          name: 'age',
+          rules: {required: true, errorMessage: '请输入正确的年龄',min:0}
         },
-        bindHeightInput: function (e: any) {
-            const value = e.detail.value;
-
-            if (!isValidNumber(value)) {
-                wx.showToast({
-                  title: '请输入正确的身高',
-                  icon: 'none'
-                });
-                return;
+        {
+          name: 'height',
+          rules: [{required: true, errorMessage: '请输入正确的身高'}, {
+            validator: (rule:any, value:any) => {
+              if(!isValidNumber(value)) {
+                return rule.message;
               }
-            this.setData({
-                height: e.detail.value
-            });
+            }, message: '身高格式不正确'
+          }]
         },
-        bindAgeInput: function (e:any) {
-            const value = e.detail.value;
-
-            if (!isValidNumber(value)) {
-                wx.showToast({
-                  title: '请输入正确的年龄',
-                  icon: 'none'
-                });
-                return;
+        {
+          name: 'weight',
+          rules: [{required: true, errorMessage: '请输入正确的体重'}, {
+            validator: (rule:any, value:any) => {
+              if(!isValidNumber(value)) {
+                return rule.message;
               }
-            this.setData({
-                age: e.detail.value
-            });
+            }, message: '体重格式不正确'
+          }]
         },
-        bindGripStrengthInput: function (e:any) {
-            const value = e.detail.value;
-
-            if (!isValidNumber(value)) {
-                wx.showToast({
-                  title: '请输入正确的握力',
-                  icon: 'none'
-                });
-                return;
+        {
+          name: 'heartRate',
+          rules: [{required: true, errorMessage: '请输入正确的心率'}, {
+            validator: (rule:any, value:any) => {
+              if(!isValidNumber(value)) {
+                return rule.message;
               }
-            this.setData({
-                gripStrength: e.detail.value
-            });
+            }, message: '心率格式不正确'
+          }]
         },
-        bindHeartRateInput: function (e:any) {
-            const value = e.detail.value;
-
-            if (!isValidNumber(value)) {
-                wx.showToast({
-                  title: '请输入正确的心率',
-                  icon: 'none'
-                });
-                return;
+        {
+          name: 'gripStrength',
+          rules: [{required: true, errorMessage: '请输入正确的握力'}, {
+            validator: (rule:any, value:any) => {
+              if(!isValidNumber(value)) {
+                return rule.message;
               }
-            this.setData({
-                heartRate: e.detail.value
-            });
+            }, message: '握力格式不正确'
+          }]
         },
-        selectGender: function(e:any) {
-            this.setData({
-              gender: e.detail.value
-            });
-          },
+        {
+            name: 'gender',
+            rules: [{required: true, errorMessage: '请输入正确的性别'}, {
+              validator: (rule:any, value:any) => {
+                if(value!=='male' && value!=='female') {
+                  return rule.message;
+                }
+              }, message: '性别格式不正确'
+            }]
+          }
+      ],
+  },
+
+  radioChange: function (e:any) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    const items = this.data.items;
+    for (let i = 0, len = items.length; i < len; ++i) {
+        items[i].checked = items[i].value == e.detail.value;
+    }
+    // console.log(radioItems);
+    // console.log(e.detail.value);
+    this.setData({
+        items: items,
+        [`form.gender`]: e.detail.value
+    });
+},
+
+  
+
+formInputChange(e:any) {
+    const {field} = e.currentTarget.dataset
+    this.setData({
+      [`form.${field}`]: e.detail.value
+    })
+  },
+closeDialog(e:any) {
+    this.setData({
+      showDialog: false
+    })
+  },
+
+     
+    
         calculate: function() {
-            const age = this.data.age;
-            const height = this.data.height;
-            const weight = this.data.weight;
-            const heartRate = this.data.heartRate;
-            const gripStrength = this.data.gripStrength;
-            const gender = this.data.gender;
+            const age = parseFloat(this.data.form.age);
+            const height = parseFloat(this.data.form.height);
+            const weight = parseFloat(this.data.form.weight);
+            const heartRate = parseFloat(this.data.form.heartRate);
+            const gripStrength = parseFloat(this.data.form.gripStrength);
+            // this.data.items.
+            const gender = this.data.form.gender;
 
-            // 计算不同公式的结果并更新页面数据
+            console.log(age,
+                height,weight,heartRate,gripStrength,gender)
+
+            this.selectComponent('#form').validate((valid:Boolean, errors:any) => {
+                if(valid) {
+                    
+                               // 计算不同公式的结果并更新页面数据
             const formulas = this.data.formulas.map(item => {
                 let result = 0;
                 switch (item.name) {
@@ -139,13 +177,25 @@ Component({
             });
             // 更新页面数据
             this.setData({
-              formulas: formulas
+              formulas: formulas,
+              showDialog: true
             });
+    
+                } else {
+                    const firstError = Object.keys(errors)
+                    if(firstError.length) {
+                        wx.showToast({
+                            title: errors[firstError[0]].message,
+                            icon: 'none'
+                        })
+                    }
+                }
+            })
           },
    // 计算1BJH-ZHW1公式
-   calculate1BJHZHW1: function(weight: number, age: number, gripStrength: number, bmp: number) {
-    let res = 794.847 + 8.661 * weight - 7.976 * age + 14.757 * gripStrength + 5.037 * bmp
-    return parseFloat(res.toFixed(2));
+   calculate1BJHZHW1: function(weight: number, age: number, gripStrength: number, heartRate: number) {
+    let res = 794.847 + 8.661 * weight - 7.976 * age + 14.757 * gripStrength + 5.037 * heartRate;
+    return parseFloat(res.toFixed(0));
   },
   // 计算Alemán 公式
   calculateAleman: function(weight: number, gender: string) {
@@ -155,7 +205,7 @@ Component({
     } else {
        res =  (1644.7 + 57.14 * weight) * 0.2389;
     }
-    return parseFloat(res.toFixed(2));
+    return parseFloat(res.toFixed(0));
   },
   // 计算Lührmann1 公式
   calculateLuhrmann1: function(weight: number, age: number, gender: string) {
@@ -165,7 +215,7 @@ Component({
     } else {
       res =  (3169 + 50.0 * weight - 15.3 * age) * 0.2389;
     }
-    return parseFloat(res.toFixed(2));
+    return parseFloat(res.toFixed(0));
   },
   // 计算Harris-Benedict（HB公式）
   calculateHarrisBenedict: function(weight: number, age: number, height: number, gender: string) {
@@ -175,7 +225,7 @@ Component({
     } else {
       res =  665.0995 + 9.5634 * weight + 1.8496 * height - 4.6756 * age;
     }
-    return parseFloat(res.toFixed(2));
+    return parseFloat(res.toFixed(0));
   },
   // 计算FAO-WHO-UNU（WHO公式）
   calculateFAOWHOUNU: function(weight: number, height: number, gender: string) {
@@ -185,7 +235,7 @@ Component({
     } else {
       res = (38.5 * weight + 26.652 * height - 1264) / 4.186;
     }
-    return parseFloat(res.toFixed(2));
+    return parseFloat(res.toFixed(0));
   },
   // 计算ESPEN公式
   calculateESPEN: function(weight: number) {
@@ -196,11 +246,30 @@ Component({
   calculateCSPEN: function(weight: number,height: number) {
 
     let bmi = 10000 * weight / (height * height);
+    let res = 0;
     if (bmi < 21) {
-      return weight * 21.4;
+        res = weight * 21.4
     } else {
-      return weight * 18.4;
+        res = weight * 18.4
     }
-  }
-    }
+    return parseFloat(res.toFixed(0));
+  },
+  reset: function() {
+    this.setData({
+    form:{
+      weight: '',
+      age: '',
+      height: '',
+      gripStrength: '',
+      heartRate: '',
+      gender: ''
+    },
+    formulas:formulas,
+    items: [
+        {name: '男', value: 'male',checked:false},
+        {name: '女', value: 'female',checked:false}
+    ]
+},
+    );
+  }    
 });
